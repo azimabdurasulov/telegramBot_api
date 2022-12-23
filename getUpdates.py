9d81ed0e4c771de8c35d159a3070694bcc6a5c6c
ZARIFMAC <uzbzarif@gmail.com>
2022-12-23 10:08:04 +0500

    get updates

---
+++ getUpdates.py
@@ -0,0 +1,10 @@
+import requests
+TOKEN = '5355192064:AAEbylzYMrRsWJCqufKA6H2leOWhH3kIcdg'
+BASE_URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+
+response = requests.get(url=BASE_URL)
+updates=response.json()['result']
+ 
+for update in updates:
+    msg = update['message']
+    print(msg['from'])
