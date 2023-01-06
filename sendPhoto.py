30a54bb8c2878757448b63b70713419786241c1c
ZARIFMAC <uzbzarif@gmail.com>
2023-01-06 10:20:21 +0500

    send file

--- sendPhoto.py
+++ sendPhoto.py
@@ -6,16 +6,21 @@ TOKEN = os.environ['TOKEN']
 def sendPhoto(chat_id:str,photo:str):
     URL = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
     param = {
-        'chat_id':chat_id,
+        'chat_id':chat_id
+        # 'photo':photo
+    }
+    files = {
         'photo':photo
     }
-    r = requests.get(url=URL,params=param)
-    pass
+    r = requests.post(url=URL,params=param,files=files)
+    print(r.json())
 
 
 chat_id ='5575549228'
 
 photo_url='https://random.dog/2bff25d0-c721-4078-8cc9-f3ce6b464428.jpg'
-photo_id = 'AgACAgIAAxkBAAMcY7era1HI4YE7NaQeMpvKkDL9AgIAAnu_MRvjDsFJSqWju_4Jl98BAAMCAANzAAMtBA'
+photo_id = 'AgACAgIAAxkDAAMgY7evYvSyDJQ8DS-1S5Irjcd9cIgAAoa_MRvjDsFJ4H7lvD-PEXwBAAMCAAN4AAMtBA'
 
-sendPhoto(chat_id=chat_id,photo=photo_id)
+img = open('logo.jpg','rb').read()
+
+sendPhoto(chat_id=chat_id,photo=img)
