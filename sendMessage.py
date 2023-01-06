21269177dbc8f77f6f3134087b9b7ef9192dbe86
ZARIFMAC <uzbzarif@gmail.com>
2023-01-06 09:54:50 +0500

    send Message

--- sendMessage.py
+++ sendMessage.py
@@ -1,4 +1,16 @@
+import os
 import requests
-def sendMssage(chat_id,text):
-    pass
 
+TOKEN = os.environ['TOKEN']
+
+def sendMessage(chat_id:str,text:str):
+    URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    param = {
+        'chat_id':chat_id,
+        'text':'Salom'
+    }
+    r = requests.get(url=URL,params=param)
+    
+chat_id ='5575549228'
+
+sendMessage(chat_id=chat_id,text='Salom')
